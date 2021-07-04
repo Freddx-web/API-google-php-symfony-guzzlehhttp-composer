02bc8609aed026f2af63871007fbc63d5ddea0b2
Fredd <66976840+Freddx-web@users.noreply.github.com>
2021-07-04 11:39:10 -0400

    Update config.php

--- config.php
+++ config.php
@@ -12,13 +12,13 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID | Copiar "ID DE CLIENTE"
-$google_client->setClientId('227471344625-00lul0h4i4bas11it3v11uv8oltg7jef.apps.googleusercontent.com');
+$google_client->setClientId('---------> ID DE CLIENTE <---------');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('5zefjdDcFIRVfMAhwMFgujwq');
+$google_client->setClientSecret('---------> SECRET KEY <---------');
 
 //Set the OAuth 2.0 Redirect URI | URL AUTORIZADO
-$google_client->setRedirectUri('http://localhost/WWW/PHP%20-%20METODOS%20Y%20FUNCIONES/PHP%20-%20API%20Gmail%20-%20Google/PHP%20-%20Login-con-la-API-de-google-master/index.php');
+$google_client->setRedirectUri('---------> URL AUTORIZADO <---------');
 
 // to get the email and profile 
 $google_client->addScope('email');
